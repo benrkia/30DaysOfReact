b8930bcdae75ae4867ab0e6c329d930d7b2d2fbd
benrkia ilyasse <benrkyailyass@gmail.com>
2019-02-21 17:54:56 +0000

    adjust day 2 challenge

--- Code/3rd_challenge/solution.js
+++ Code/3rd_challenge/solution.js
@@ -6,7 +6,6 @@ const items = [
     {title: 'item 5', url: 'https://google.com/'},
 ];
 
-<<<<<<< HEAD
 const Item = (props) => {
     const {item} = props;
     return (
@@ -15,13 +14,6 @@ const Item = (props) => {
         </li>
     );
 }
-=======
-const itemsElements = items.map(item =>
-    <li>
-        {item.title} <a href={item.url}>visit</a>
-    </li>
-);
->>>>>>> a63dee88f1d3c4d1c79b173967891160d6be6936
 
 const headerProps = {
     className: 'hidden',
@@ -35,13 +27,9 @@ const element = (
     <div>
         <header {...headerProps} className='container'  />
         <ul>
-<<<<<<< HEAD
             {
                 items.map(item => <Item item={item} /> )
             }
-=======
-            {itemsElements}
->>>>>>> a63dee88f1d3c4d1c79b173967891160d6be6936
         </ul>
     </div>
 );
